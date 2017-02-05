2ce5189e7c72f98201c49ca8c1afca4bf75cd163
Faysal Sharif <fxsrt10@gmail.com>
2017-02-05 17:41:52 -0600

    Added bit controller

---
+++ HoloLens/LearnBinary/Unity/LearnBinary/Assets/LearnBinary/Scripts/BitController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitController : MonoBehaviour {
+    private bool _isOn = false;
+    public float power;
+    public float value;
+
+
+    public Light pointLight;
+    public Animator onOff;
+
+    public Material on;
+    public Material off;
+    public GameObject filament;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public bool isOnValue
+    {
+        get { return _isOn; }
+        set
+        {
+            if(_isOn != onOff.GetBool("toggleSwitch"))
+            {
+                _isOn = onOff.GetBool("toggleSwitch");
+                if (_isOn)
+                {
+                    CalculateValue();
+                    filament.GetComponent<Renderer>().material = on;
+                }
+                else
+                {
+                    this.value = 0f;
+                    filament.GetComponent<Renderer>().material = off;
+                }
+            }
+        }
+    }
+
+    public void CalculateValue()
+    {
+        value = Mathf.Pow(2f, power);
+    }
+}
