cd3a7b663cd0c4fe888cc6bb9c77c9ccf346e00a
Jared Bienz <jbienz@hotmail.com>
2017-02-05 19:00:07 -0600

    Started the BitManager. More progress on the LearningController.

---
+++ HoloLens/LearnBinary/Unity/LearnBinary/Assets/LearnBinary/Scripts/BitManager.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages an array of bit blocks.
+/// </summary>
+public class BitManager : MonoBehaviour
+{
+	#region Member Variables
+	private int totalValue;
+	#endregion // Member Variables
+
+	#region Inspector Fields
+	[Tooltip("The total bits used in the scene.")]
+	public BitController[] bits;
+	#endregion // Inspector Fields
+
+	#region Behavior Overrides
+	void Start()
+	{
+		// Validate
+		if ((bits == null) || (bits.Length < 1))
+		{
+			Debug.LogErrorFormat("The {0} inspector field is not set and is required. {1} did not load completely.", "bits", this.GetType().Name);
+			return;
+		}
+
+		// Subscribe to events
+		foreach (BitController bit in bits)
+		{
+			bit.ValueChanged += Bit_ValueChanged;
+		}
+	}
+
+	private void Bit_ValueChanged(object sender, EventArgs e)
+	{
+		// Figure out total value
+		totalValue = 0;
+		foreach (BitController bit in bits)
+		{
+			totalValue += (int)bit.Value;
+		}
+
+		// Notify
+		if (TotalValueChanged != null) { TotalValueChanged(this, EventArgs.Empty); }
+	}
+	#endregion // Behavior Overrides
+
+	#region Public Properties
+	/// <summary>
+	/// Gets the total value of all current bits.
+	/// </summary>
+	public int TotalValue
+	{
+		get
+		{
+			return totalValue;
+		}
+		private set
+		{
+			if (totalValue != value)
+			{
+				totalValue = value;
+				if (TotalValueChanged != null) { TotalValueChanged(this, EventArgs.Empty); }
+			}
+		}
+	}
+	#endregion // Public Properties
+
+	#region Public Events
+	/// <summary>
+	/// Occurs whenever the <see cref="TotalValue"/> property has changed.
+	/// </summary>
+	public event EventHandler TotalValueChanged;
+	#endregion // Public Events
+}
--- HoloLens/LearnBinary/Unity/LearnBinary/Assets/LearnBinary/Scripts/LearningController.cs
+++ HoloLens/LearnBinary/Unity/LearnBinary/Assets/LearnBinary/Scripts/LearningController.cs
@@ -1,16 +1,90 @@
-﻿using System.Collections;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.SpatialMapping;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LearningController : MonoBehaviour {
+public enum LearningState
+{
+	Placing,
+	Interacting,
+}
 
-	// Use this for initialization
-	void Start () {
-		
+public class LearningController : Singleton<LearningController>
+{
+	#region Member Variables
+	private LearningState state;
+	#endregion // Member Variables
+
+	#region Inspector Fields
+	[Tooltip("The bit manager the scene.")]
+	public BitManager bitManager;
+
+	[Tooltip("The cursor to be used with the Directional Indicator.")]
+	public GameObject cursor;
+
+	[Tooltip("The TapToPlace used to place the whole scene.")]
+	public TapToPlaceEx scenePlacement;
+	#endregion // Inspector Fields
+
+	#region Behavior Overrides
+	void Start()
+	{
+		// Validate components
+		if (bitManager == null)
+		{
+			Debug.LogErrorFormat("The {0} inspector field is not set and is required. {1} did not load completely.", "bitManager", this.GetType().Name);
+			return;
+		}
+		if (cursor == null)
+		{
+			Debug.LogErrorFormat("The {0} inspector field is not set and is required. {1} did not load completely.", "cursor", this.GetType().Name);
+			return;
+		}
+		if (scenePlacement == null)
+		{
+			Debug.LogErrorFormat("The {0} inspector field is not set and is required. {1} did not load completely.", "scenePlacement", this.GetType().Name);
+			return;
+		}
+
+		// Subscribe to events
+		bitManager.TotalValueChanged += BitManager_TotalValueChanged;
+		scenePlacement.PlacingCompleted += ScenePlacement_PlacingCompleted;
 	}
-	
+
+	private void BitManager_TotalValueChanged(object sender, System.EventArgs e)
+	{
+		// Update the total block
+	}
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
 	}
+	#endregion // Behavior Overrides
+
+	#region Overrides / Event Handlers
+	private void ScenePlacement_PlacingCompleted(object sender, System.EventArgs e)
+	{
+		// If we're in placing, transition to interacting
+		if (state == LearningState.Placing)
+		{
+			state = LearningState.Interacting;
+		}
+	}
+	#endregion // Overrides / Event Handlers
+
+	#region Public Properties
+	/// <summary>
+	/// Gets the current state of the learning controller.
+	/// </summary>
+	public LearningState State
+	{
+		get
+		{
+			return state;
+		}
+	}
+	#endregion // Public Properties
 }
